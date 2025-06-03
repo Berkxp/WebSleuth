cb27c99c55a7bfa813a4b4c5ca8f3f885cbcfffa
Berkxp <thexz1506@gmail.com>
2025-06-02 22:47:19 -0300

    start.py fixed

--- core/start.py
+++ core/start.py
@@ -9,11 +9,11 @@ sys = platform.system()
 parse = argparse.ArgumentParser(description="[+] Tool for Web Scan")
 
 parse.add_argument("-u", "--url", type=str, help="Url that will be scanned.")
-parse.add_argument("-sS", "--subdomain-scan", action="store_true", help="Search for interesting dir's in url.")
+parse.add_argument("-s", "--subdomain-scan", action="store_true", help="Search for interesting dir's in url.")
 parse.add_argument("-wL", "--whois-lookup", action="store_true", help="Make a Whois Lookup on url.")
 parse.add_argument("-nS", "--nmap", action="store_true", help="Make a Nmap Scan in Site's IP.")
 parse.add_argument("-o", "--osint", action="store_true", help="Osint the Nickname.")
-parse.add_argument("-sS", "--sqli-scan", action="store_true", help="Scan for known vulnerabilities on the target URL.")
+parse.add_argument("-S", "--sqli-scan", action="store_true", help="Scan for known vulnerabilities on the target URL.")
 args = parse.parse_args()
 
 def start():
